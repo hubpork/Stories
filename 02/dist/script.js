c13b96ebf690e6db39e9e65fa2091da4cbfdffea
Michel Maillard <michel.maillard@mysign.ch>
2023-07-17 16:13:06 +0200

    Beautify Storie

--- 02/dist/script.js
+++ 02/dist/script.js
@@ -12,3 +12,25 @@ function toggleNavigation(){
 
 productImages.forEach(image => image.addEventListener("click", changeImage));
 navItem.addEventListener('click', toggleNavigation);
+
+
+
+const spinner =
+  '<svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24"><use href="#icon.spinner"></use></svg>';
+
+const states = {
+  idle: "Add To Cart",
+  sending: `${spinner} working...`,
+  done: "Done!"
+};
+
+demo.onclick = () => {
+  setState("sending");
+  setTimeout(() => setState("done"), 4000);
+  setTimeout(() => setState("idle"), 6000);
+};
+
+function setState(state) {
+  if (!document.startViewTransition) demo.innerHTML = states[state];
+  else document.startViewTransition(() => (demo.innerHTML = states[state]));
+}
